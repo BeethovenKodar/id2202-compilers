db1acb01700c85ed45fa88ab3a3b5d2296c83e79
Ludvig Larsson <ludvig19@icloud.com>
2022-11-27 16:58:14 +0100

    second subtask attempt #module2-asm-s

--- solutions/module2/asm/main-asm.asm
+++ solutions/module2/asm/main-asm.asm
@@ -15,6 +15,7 @@ help_msg5       db      "  +   Adds together arg1 and arg2", 0xA
 help_msg6       db      "  !   Prints out the factorial value of", 0xA
 help_msg7       db      "      arg1, as well as the message in arg2", 0xA, 0
 
+; str				resb	2*64	; 2 pointers of size 64
 
 ; place char in 'dil' reg
 ; push 	rdi
@@ -27,7 +28,7 @@ main:
 	mov     rbp, rdi        	; argc in rdi (64bit). Save in rbp (64bit)
 	mov     rbx, rsi        	; argv in rsi (64bit). Save in rbx (64bit)
 
-	cmp		rbp, 0x3			; if rbp < 3 jump to exit label
+	cmp		rbp, 0x4			; if rbp < 3 jump to exit label
 	jl	exit_menu
 
 	mov 	r10, [rbx + 8]		; 'r10' contains pointer to first program argument
@@ -53,7 +54,16 @@ main:
 
 
 echo:
-	ret
+	mov 	r10, [rbx + 16]		; 'r10' is argument 1 pointer
+	mov		r11, [rbx + 24] 	; 'r11' is argument 2 pointer
+	mov		rdi, r10
+	push 	r11
+	call	puts
+	pop 	r11
+	mov 	rdi, r11
+	call	puts
+	xor 	eax, eax
+	add		eax, 0
 
 
 magic:
