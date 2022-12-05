985360729dab89e2197fb51cdadf4c24a8ecd3bf
Ludvig Larsson <ludvig19@icloud.com>
2022-12-05 16:57:25 +0000

    close to done refactoring

--- solutions/cigrid/src/spilling.ml
+++ solutions/cigrid/src/spilling.ml
@@ -28,12 +28,12 @@ with
 | Not_found ->
   begin match key with
   | Reg(n) ->
-    let key = sprintf "reg_%d" n in
+    let key1 = sprintf "reg_%d" n in
     print_endline ("didn't find " ^ key ^ "!");
     let new_env = ((key, curr_offset)::env) in
     (curr_offset, curr_offset + 8, new_env)
   | TReg(n, _) ->
-    let key = sprintf "treg_%d" n in
+    let key1 = sprintf "treg_%d" n in
     print_endline ("didn't find " ^ key ^ "!");
     let new_env = ((key, curr_offset)::env) in
     (curr_offset, curr_offset + 8, new_env)
@@ -52,18 +52,30 @@ let on_stack offset =
     sprintf "qword [rsp + %d]" offset
 
 (* does it matter which operand that is moved to a register? *)
-let use_temp_reg op1 op2 = 
+(* second operand shall be moved to a tempirary register *)
+let bop_mem_mem op1 op2 curr_offset env = 
+  let (offset_op1, curr_offset1, env1) = 
+    offsets_from_env curr_offset env op2 in
+  let (offset_op2, curr_offset2, env2) = 
+    offsets_from_env curr_offset1 env1 op1 in
+  let mem_to_reg = sprintf "%s\t%s, %s" "mov" temp_reg (on_stack offset_op2) in
+  let bop_str = sprintf "%s\t%s, %s" (p_bop bop) (on_stack offset_op1) temp_reg in
+  let res_str = sprintf "%s\n%s" mem_to_reg bop_str in
+  (res_str, curr_offset2, env2)
+
+let bop_mem_imm op curr_offset env = 
+  let (offset_op, curr_offset1, env1) = 
+    offsets_from_env curr_offset env op in
+  let bop_str = sprintf "%s\t%s, %d" (p_bop bop) (on_stack offset_op) value in
+   (bop_str, curr_offset1, env1)
 
 let spill_instr curr_offset env = function
-| BinOp(bop, Reg(n1), TReg(n2, name)) ->
-  let (offset_op1, curr_offset1, env1) = 
-    offsets_from_env curr_offset env (TReg(n2, name)) in
-  let (offset_op2, curr_offset2, env2) = 
-    offsets_from_env curr_offset1 env1 (Reg(n1)) in
-  let tmp_reg = sprintf "%s\t%s, %s" "mov" temp_reg (on_stack offset_op1) in
-  let bop_str = sprintf "%s\t%s, %s" (p_bop bop) (on_stack offset_op2) temp_reg in
-  let res_str = sprintf "%s\n%s" tmp_reg bop_str in
-    (res_str, curr_offset2, env2) (* ret *)
+| BinOp(bop, op, Imm(value)) ->
+  bop_mem_imm op Imm(value) curr_offset env
+| BinOp(bop, op1, op2) ->
+  bop_mem_imm op1 op2 curr_offset env
+  
+  
 | BinOp(bop, TReg(n1, name), Reg(n2)) ->
   let (offset_op1, curr_offset1, env1) = 
     offsets_from_env curr_offset env (TReg(n1, name)) in
